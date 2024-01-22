a1cdfe027d13f86e0a718e47e79eb8b3482a9b66
Fabrice Niyongabo <ninga.fab@gmail.com>
2024-01-22 12:14:46 +0200

    app nav

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <nav className="text-white container md:w-[80%] mx-auto py-5 flex items-center justify-center">
+      <div className="text-4xl font-semibold">
+        <span>Live</span>
+        <span className="text-yellow-500 italic">Crypto</span>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
